9853e61f05dcc2b9c8b9f5a892ac314364a8c12f
Jeffy Chen <jeffy.chen@rock-chips.com>
2020-07-24 16:57:22 +0800

    Rename libraries
    libmali-<gpu>-<ver>[-subver][-platform].so
    
    For example,
    ./parse_name.sh lib/aarch64-linux-gnu/libmali-bifrost-g31-rxp0-without-cl-wayland.so
    name=libmali-bifrost-g31-rxp0-without-cl-wayland
    gpu=bifrost-g31
    version=rxp0
    subversion=without-cl
    platform=wayland
    
    Change-Id: I6d332bc5fe45665754cc7b9713617f038c216d55
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

---
+++ parse_name.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+PRINT_GPU=false
+PRINT_VERSION=false
+PRINT_SUBVERSION=false
+PRINT_PLATFORM=false
+
+parse_name() {
+	[ -z "$1" ] && return
+
+	GPU=$(echo $1|cut -d'-' -f'2,3')
+	VERSION=$(echo $1|cut -d'-' -f4)
+
+	PLATFORM=$(echo $1 | grep -owE "gbm|wayland|x11|only-cl")
+	[ -z "$PLATFORM" ] && PLATFORM=x11
+
+	SUBVERSION=$(echo ${1%-$PLATFORM}|cut -d'-' -f'5-')
+
+	if $PRINT_GPU;then
+		echo $GPU
+	elif $PRINT_VERSION;then
+		echo $VERSION
+	elif $PRINT_SUBVERSION;then
+		echo $SUBVERSION
+	elif $PRINT_PLATFORM;then
+		echo $PLATFORM
+	else
+		echo name=$1
+		echo gpu=$GPU
+		echo version=$VERSION
+		echo subversion=$SUBVERSION
+		echo platform=$PLATFORM
+	fi
+}
+
+case "$1" in
+	--gpu)
+		PRINT_GPU=true
+		shift
+		;;
+	--version)
+		PRINT_VERSION=true
+		shift
+		;;
+	--subversion)
+		PRINT_SUBVERSION=true
+		shift
+		;;
+	--platform)
+		PRINT_PLATFORM=true
+		shift
+		;;
+esac
+
+for lib in "$@";do
+	parse_name $(echo $lib|grep -o "libmali-[^\.]*")
+done
