c861959ac5667d05eb0418b608f9be7e6f6ff082
Jeffy Chen <jeffy.chen@rock-chips.com>
2021-08-19 14:58:59 +0800

    meson: Support optimize-level option
    The default optimize level is O3, there're more to come.
    
    Change-Id: I4671d9664a565c43300ec46a0aa6ac91661bd607
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- grabber.sh
+++ grabber.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 ARCH=${1:-aarch64}
 GPU=${2:-midgard-t86x}
 VERSION=${3:-r18p0}
 SUBVERSION=${4:-none}
 PLATFORM=${5:-x11}
+OPTIMIZE=${6:-O3}
 
 [ ${ARCH} = 'armv7l' -o ${ARCH} = 'armhf' -o ${ARCH} = 'arm32' ] && ARCH=arm
 [ ${ARCH} = 'armv8' -o ${ARCH} = 'arm64' ] && ARCH=aarch64
@@ -17,4 +18,11 @@ else
 	LIB="libmali-${GPU}-${VERSION}-${SUBVERSION}-${PLATFORM}.so"
 fi
 
-find lib/${ARCH}* -name ${LIB} 2>/dev/null
+DIR=lib
+case ${OPTIMIZE} in
+	O*)
+		DIR=optimize_${OPTIMIZE#O}
+		;;
+esac
+
+find ${DIR}/${ARCH}* -name ${LIB}
